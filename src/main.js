8d43a348dd5ce9bb64010d733783ee688b8f50e3
Lorenzi <8540358+hlorenzi@users.noreply.github.com>
2022-08-19 15:35:16 -0300

    update dependencies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import ReactDOMClient from "react-dom/client"
 import App from "./App.tsx"
 
 
 document.body.onload = function()
 {
-	ReactDOM.render(<App/>, document.getElementById("divApp"))
+	const container = document.getElementById("divApp")
+	const root = ReactDOMClient.createRoot(container)
+	root.render(<App/>)
 }
